9025aa36afb5ff7363f095ee08852a23a5c0f26a
Urbano Gardun <urbanogardun@gmail.com>
2018-05-23 21:07:23 +0200

    Add logout mechanism

--- app/javascript/components/Logout.js
+++ app/javascript/components/Logout.js
@@ -1,14 +1,48 @@
 import React from "react"
 import PropTypes from "prop-types"
+import axios from 'axios'
 
 class Logout extends React.Component {
-    render () {
-      return (
-        <React.Fragment>
-          <h2>Logout</h2>
-        </React.Fragment>
-      );
+
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
     }
+
+    handleLogout(e) {
+        e.preventDefault();
+        let that = this
+        let email = this.props.currentUser
+        
+        let instance = axios.create({
+            headers: {'X-CSRF-Token' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')}
+        });
+        instance.delete('/users/sign_out', {
+        })
+        .then(function (response) {
+            console.log(response);
+            that.props.history.push('/');
+            // that.props.changePage("login")
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleLogout}>Sign Out</button>
+        );
+    };
+
+
+    // render () {
+    //   return (
+    //     <React.Fragment>
+    //       <h2>Logout</h2>
+    //     </React.Fragment>
+    //   );
+    // }
   }
   
   export default Logout
--- app/javascript/components/TodoManager.js
+++ app/javascript/components/TodoManager.js
@@ -1,11 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Logout from './Logout'
 
 class TodoManager extends React.Component {
     render () {
       return (
         <React.Fragment>
           <h2>TodoManager</h2>
+          <Logout {...this.props} />
         </React.Fragment>
       );
     }
