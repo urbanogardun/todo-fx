983da7519c859d51d05834489d086a5b666bd748
Urbano Gardun <urbanogardun@gmail.com>
2018-05-25 12:28:27 +0200

    Fetch todos in order created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,7 +6,7 @@ class TodosController < ApplicationController
     # GET /todos/:user_id
     def show
         # Id 23 is of user bloom@gmail.com
-        todos = Todo.where user_id: params[:user_id]
+        todos = Todo.where(user_id: params[:user_id]).order(created_at: :desc)
         render json: todos
     end
 
