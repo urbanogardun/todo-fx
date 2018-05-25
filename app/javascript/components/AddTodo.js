ee9adc4e3de7aeabb0c91d9eddf98c1441acdc4b
Urbano Gardun <urbanogardun@gmail.com>
2018-05-25 09:11:46 +0200

    Style addTodo component

--- app/javascript/components/AddTodo.js
+++ app/javascript/components/AddTodo.js
@@ -25,37 +25,64 @@ class AddTodo extends React.Component {
     }
 
     render() {
-        // Have 3 input fields: single line of text, due date & priority
         return (
             <React.Fragment>
-                <h2>AddTodo</h2>
-                <div className="field">
-                    <label className="label">Todo</label>
-                    <div className="control">
-                        <input className="input is-primary" id="add-todo__input-subject" type="text" placeholder="Subject" />
-                    </div>
-                </div>
-                <div className="field">
-                    <label className="label">Due Date</label>
-                    <div className="control">
-                        <input className="input is-primary" id="add-todo__input-due-date" type="text" placeholder="mm/dd/yyyy hh:mm:ss" />
-                    </div>
-                </div>
-                <div className="field">
-                    <label className="label">Priority</label>
-                    <div className="control">
-                        <div className="select is-primary">
-                            <select id="add-todo__select-priority">
-                                <option value="1">1 (Highest)</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
+                <div className="container">
+
+                    <div className="field">
+                        <label className="label is-medium">Subject</label>
+                        <div className="control has-icons-left has-icons-right">
+                            <input className="input is-medium" id="add-todo__input-subject" type="text" placeholder="Text input" />
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-envelope fa-xs"/>
+                            </span>
+                            <span className="icon is-small is-right">
+                                <i className="fas fa-check fa-xs"/>
+                            </span>
                         </div>
                     </div>
+
+                    <div className="field">
+                        <label className="label is-medium">Due Date</label>
+                        <div className="control has-icons-left has-icons-right">
+                            <input className="input is-medium" id="add-todo__input-due-date" type="text" placeholder="MM/DD/YYYY HH:MM:SS" />
+                            <span className="icon is-left">
+                                <i className="fas fa-envelope fa-sm" />
+                            </span>
+                            <span className="icon is-right">
+                                <i className="fas fa-check fa-sm" />
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="field is-grouped">
+                        <p className="control has-icons-left">
+                            <label className="label is-medium">Priority</label>
+                            <span className="select is-medium">
+                                <select id="add-todo__select-priority">
+                                    <option value="1">1 (Highest)</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                            </span>
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-globe"/>
+                            </span>
+                        </p>
+
+
+                        <div className="control">
+                            <label className="label is-medium" style={{visibility: 'hidden'}}>Add</label>
+                            <button className="button is-primary is-medium" onClick={this.addTodo}>Add</button>
+                        </div>
+                    </div>
+
                 </div>
-                <button onClick={this.addTodo}>Add Todo</button>
+
+                <br />
+
             </React.Fragment>
         );
     }
